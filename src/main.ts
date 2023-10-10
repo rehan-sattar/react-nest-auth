802ac65bc14a55ebcfc7ebd4dee46ca8d9d0420b
Rehan  Sattar <rehansattar117@gmail.com>
2023-10-10 18:37:26 +0500

    📦 NEW: Add documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Authentication Server')
+    .setDescription(
+      'This server contains services and apis for performing authentication options. ',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
