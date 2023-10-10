fecffd716d38a5716253350e22caedb7618fb3fd
Rehan  Sattar <rehansattar117@gmail.com>
2023-10-10 20:04:31 +0500

    📦 NEW: Create base for test cases of authentication

--- src/modules/authentication/__test__/authentication.controller.spec.ts
+++ src/modules/authentication/__test__/authentication.controller.spec.ts
@@ -1,20 +1,52 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthenticationController } from '../authentication.controller';
+import { Connection, connect, Model } from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+import { HashingService } from '../hashing/hashing.service';
+import { User, UserSchema } from '../../users/users.schema';
 import { AuthenticationService } from '../authentication.service';
+import { AuthenticationController } from '../authentication.controller';
 
 describe('AuthenticationController', () => {
+  let userModel: Model<User>;
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
   let controller: AuthenticationController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    userModel = mongoConnection.model(User.name, UserSchema);
+
+    const app: TestingModule = await Test.createTestingModule({
       controllers: [AuthenticationController],
-      providers: [AuthenticationService],
+      providers: [
+        AuthenticationService,
+        HashingService,
+        { provide: getModelToken(User.name), useValue: userModel },
+      ],
     }).compile();
 
-    controller = module.get<AuthenticationController>(AuthenticationController);
+    controller = app.get<AuthenticationController>(AuthenticationController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterAll(async () => {
+    await mongoConnection.dropDatabase();
+    await mongoConnection.close();
+    await mongod.stop();
+  });
+
+  afterEach(async () => {
+    const collections = mongoConnection.collections;
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
+    }
+  });
+
+  describe('Authentication Controller', () => {
+    it('should do something!', () => {});
   });
 });
