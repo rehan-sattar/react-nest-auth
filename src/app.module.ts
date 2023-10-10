526d3c528196df2f3a91cfc7cf7125e5b3c85e54
Rehan  Sattar <rehansattar117@gmail.com>
2023-10-10 18:28:12 +0500

    📦 NEW: Establish db connection with dev config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './modules/database/database.module';
+import { ConfigurationModule } from './modules/config/config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigurationModule, DatabaseModule],
 })
 export class AppModule {}
