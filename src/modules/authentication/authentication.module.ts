a272c32d2ae5bf64678df5702a5ffe5b800d454b
Rehan  Sattar <rehansattar117@gmail.com>
2023-10-10 19:34:49 +0500

    📦 NEW: Complete basic sign-up flow

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import { User, UserSchema } from '../users/users.schema';
+import { HashingService } from './hashing/hashing.service';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [HashingService, AuthenticationService],
 })
 export class AuthenticationModule {}
--- src/modules/authentication/authentication.service.ts
+++ src/modules/authentication/authentication.service.ts
@@ -1,11 +1,36 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { User } from '../users/users.schema';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { HashingService } from './hashing/hashing.service';
+import { UserAlreadyExists } from './exceptions/user-already-exists.exception';
 
 @Injectable()
 export class AuthenticationService {
+  @InjectModel(User.name) private userModel: Model<User>;
+
+  constructor(private readonly hashingService: HashingService) {}
+
   async signUp(signUpDto: SignUpDto) {
-    console.log(signUpDto);
+    const { name, email, password } = signUpDto;
+
+    const userExists = await this.userModel.findOne({ email });
+
+    if (userExists) throw new UserAlreadyExists();
+
+    const salt = await this.hashingService.getSalt();
+    const hashedPassword = await this.hashingService.hash(password, salt);
+
+    const user = await new this.userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
   }
 
   async signIn(signInDto: SignInDto) {
